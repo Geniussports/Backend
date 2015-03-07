98398483946a94a31e9cbec4fba67121619e7c5b
Alan Matthews <matthewsa1320@gmail.com>
2015-03-07 18:05:08 -0500

    Adding passwords_controller with update method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations",
-                                    sessions: "sessions" }
+                                    sessions: "sessions",
+                                    passwords: "passwords" }
 
 
   resources :teams, only: [:create, :update, :show]
